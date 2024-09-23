dca859671262574692e4ea26dadc6efb589dea0e
AritraChakraborty <caritra838@gmail.com>
2024-09-23 14:53:47 +0600

    added login feature

--- sharenotes/base/urls.py
+++ sharenotes/base/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('register/', views.register, name='register'),
 ]
--- sharenotes/base/views.py
+++ sharenotes/base/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 # Create your views here.
 
+@login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'user': request.user
+    }
+    return render(request, 'home.html', context)
 
 
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username, password)
 
         try:
             user = User.objects.get(username=username)
@@ -34,6 +38,9 @@ def loginPage(request):
 
     return render(request, 'login.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def register(request):
     return render(request, 'registration.html')
